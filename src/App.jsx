11dcf8ac710d9423c1c68d045aa4396ee6294e65
SimonPercy <mmmRaliphada809@gmail.com>
2023-11-14 23:06:09 +0200

    reverted back to the original

--- src/App.jsx
+++ src/App.jsx
@@ -1,112 +1,98 @@
-import React, { useEffect, useRef, useReducer } from "react";
+import { useEffect, useRef, useState } from "react";
 import "./App.css";
 
-const initialState = {
-  breakMins: 5,
-  initialMins: 25,
-  mins: 25,
-  secs: 0,
-  timing: true,
-  pause: false,
-};
-
-const reducer = (state, action) => {
-  switch (action.type) {
-    case "TOGGLE_TIMER":
-      return { ...state, pause: !state.pause };
-    case "HANDLE_TIMEOUT":
-      // Reducer logic for handling timeouts
-      // ...
-      const newStateTimeout = { ...state }; // Initialize newState based on current state
-      // Update newState properties as needed
-      // ...
-      return newStateTimeout;
-    case "RESET":
-      // Reducer logic for resetting
-      // ...
-      const newStateReset = { ...state }; // Initialize newState based on current state
-      // Update newState properties as needed
-      // ...
-      return newStateReset;
-    case "BREAK_DECREMENT":
-      // Reducer logic for break decrement
-      // ...
-      const newStateBreakDecrement = { ...state }; // Initialize newState based on current state
-      // Update newState properties as needed
-      // ...
-      return newStateBreakDecrement;
-    case "DECREMENT":
-      // Reducer logic for decrement
-      // ...
-      const newStateDecrement = { ...state }; // Initialize newState based on current state
-      // Update newState properties as needed
-      // ...
-      return newStateDecrement;
-    case "BREAK_INCREMENT":
-      // Reducer logic for break increment
-      // ...
-      const newStateBreakIncrement = { ...state }; // Initialize newState based on current state
-      // Update newState properties as needed
-      // ...
-      return newStateBreakIncrement;
-    case "CHANGE_TIME":
-      // Reducer logic for changing time
-      // ...
-      const newStateChangeTime = { ...state }; // Initialize newState based on current state
-      // Update newState properties as needed
-      // ...
-      return newStateChangeTime;
-    default:
-      return state;
-  }
-};
-
 function App() {
-  const [state, dispatch] = useReducer(reducer, initialState);
+  const [breakMins, setBreakMins] = useState(5);
+  const [initialMins, setInitialMins] = useState(25);
+  const [mins, setMins] = useState(initialMins);
+  const [secs, setSecs] = useState(parseInt(mins * 60) % 60);
+
+  const [timing, setTiming] = useState(true);
+  const [pause, setPause] = useState(false);
   const audio = useRef();
 
+  const toggleTimer = () => {
+    setPause(!pause);
+  };
+  let timerTimeout;
+  const handleTimeout = () => {
+    if (pause) {
+      if (secs === 0 && mins === 0) {
+        audio.current.play();
+        if (timing) {
+          setTiming(false);
+          setMins(breakMins);
+          setSecs((breakMins * 60) % 60);
+        } else {
+          setTiming(true);
+          setMins(initialMins);
+          setSecs((initialMins * 60) % 60);
+        }
+      } else {
+        setSecs(secs - 1);
+        if (secs === 0) {
+          setSecs(59);
+          setMins(mins - 1);
+        }
+        timerTimeout = setTimeout(handleTimeout, 1000);
+      }
+    }
+  };
+  const reset = () => {
+    setInitialMins(25);
+    setBreakMins(5);
+    setMins(25);
+    setPause(false);
+    audio.current.pause();
+    audio.current.currentTime = 0;
+    setSecs((mins * 60) % 60);
+    clearTimeout(timerTimeout);
+    setTiming(true);
+  };
+  const breakdre = () => {
+    if (breakMins > 1 && !pause) {
+      setBreakMins(breakMins - 1);
+      if (!timing) {
+        setMins(breakMins - 1);
+      }
+    }
+  };
+  const decrement = () => {
+    if (!pause && mins > 1) {
+      setInitialMins(initialMins - 1);
+      setMins(initialMins - 1);
+    }
+  };
+  const breakIncre = () => {
+    if (!pause && breakMins < 60) {
+      setBreakMins(breakMins + 1);
+      if (!timing) {
+        setMins(breakMins + 1);
+      }
+    }
+  };
+  const changeTime = () => {
+    if (!pause && mins < 60) {
+      setInitialMins(initialMins + 1);
+      setMins(initialMins + 1);
+    }
+  };
+
   useEffect(() => {
-    const timerTimeout = setTimeout(() => {
-      dispatch({ type: "HANDLE_TIMEOUT" });
-    }, 1000);
+    timerTimeout = setTimeout(handleTimeout, 1000);
 
     return () => clearTimeout(timerTimeout);
-  }, [state.secs, state.pause, state.mins]);
-
-  const toggleTimer = () => {
-    dispatch({ type: "TOGGLE_TIMER" });
-  };
-
-  const reset = () => {
-    dispatch({ type: "RESET" });
-  };
-
-  const breakDecrement = () => {
-    dispatch({ type: "BREAK_DECREMENT" });
-  };
-
-  const decrement = () => {
-    dispatch({ type: "DECREMENT" });
-  };
-
-  const breakIncrement = () => {
-    dispatch({ type: "BREAK_INCREMENT" });
-  };
-
-  const changeTime = () => {
-    dispatch({ type: "CHANGE_TIME" });
-  };
+  }, [secs, pause, mins]);
 
   return (
     <div id="container">
-      {/* UI components */}
       <div id="break">
         <p id="break-label">Break Length</p>
-        <div id="break-length">{state.breakMins}</div>
-        <button id="break-increment" onClick={breakIncrement}>
+        <div id="break-length">{breakMins}</div>
+        <button id="break-increment" onClick={breakIncre}>
           ⬆️
         </button>
-        <button id="break-decrement" onClick={breakDecrement}>
+        <button id="break-decrement" onClick={breakdre}>
           ⬇️
         </button>
       </div>
@@ -122,14 +108,14 @@ function App() {
         {" "}
         Reset
       </button>
-      <div id="timer-label">{state.timing ? "session" : "break"}</div>
+      <div id="timer-label">{timing ? "session" : "break"}</div>
       <div id="time-left">
-        {(state.mins + "0").length > 2 ? state.mins : "0" + state.mins}:
-        {(state.secs + "0").length > 2 ? state.secs : "0" + state.secs}
+        {(mins + "0").length > 2 ? mins : "0" + mins}:
+        {(secs + "0").length > 2 ? secs : "0" + secs}
       </div>
       <div id="session">
         <div id="session-label">Session Length</div>
-        <div id="session-length">{state.initialMins}</div>
+        <div id="session-length">{initialMins}</div>
         <button id="session-increment" onClick={changeTime}>
           ⬆️
         </button>
